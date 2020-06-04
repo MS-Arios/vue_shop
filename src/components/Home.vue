8568ff967232bedaff3300ca2aca48e484cc6fbf
unknown <794061@qq.com>
2020-06-04 17:42:24 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      Home组件
+      <el-button type="info" v-on:click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data: function() {
+      return {}
+  },
+  methods: {
+      logout: function() {
+          window.sessionStorage.clear();
+          this.$router.push("/login")
+      }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
